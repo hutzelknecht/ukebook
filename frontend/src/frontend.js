a4905b473db9de6eefefd8e127c0ad9dfb6839b4
hutzelknecht <micha@immerdieses.de>
2015-11-14 14:50:30 +0100

    generated frontend and backend

---
+++ frontend/src/frontend.js
@@ -0,0 +1,17 @@
+import {ComponentMetadata as Component, ViewMetadata as View} from 'angular2/angular2';
+
+@Component({
+  selector: 'frontend'
+})
+
+@View({
+  templateUrl: 'frontend.html'
+})
+
+export class Frontend {
+
+  constructor() {
+    console.info('Frontend Component Mounted Successfully');
+  }
+
+}
---
+++ frontend/src/index.js
@@ -0,0 +1,19 @@
+import {ComponentMetadata as Component, ViewMetadata as View, bootstrap} from 'angular2/angular2';
+import {Frontend} from 'frontend';
+
+@Component({
+  selector: 'main'
+})
+
+@View({
+  directives: [Frontend],
+  template: `
+    <frontend></frontend>
+  `
+})
+
+class Main {
+
+}
+
+bootstrap(Main);
