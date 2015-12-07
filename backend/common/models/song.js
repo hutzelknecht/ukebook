fa726c040927a764217d394c4137178a72ddbf1f
Micha <micha@immerdieses.de>
2015-12-07 23:10:19 +0100

    fixed merge conflicts

--- backend/common/models/song.js
+++ backend/common/models/song.js
@@ -1,10 +1,5 @@
 var wkhtmltopdf = require('wkhtmltopdf');
-<<<<<<< HEAD
 
-=======
-//wkhtmltopdf.command="C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe";
-var fs = require('fs');
->>>>>>> origin/master
 module.exports = function(Song) {
   Song.pdf = function(res, callback){
 
@@ -19,31 +14,10 @@ module.exports = function(Song) {
       toc: {}
     }).pipe(res, 'binary');
 
-<<<<<<< HEAD
   };
-  Song.remoteMethod('pdf',
-    {
+  Song.remoteMethod('pdf',{
       accepts: [ { arg: 'res', type: 'object', 'http': {source: 'res'} } ],
       http: {path: '/pdf', verb: 'get'}
-    });
-=======
-	Song.pdf = function(res) {
-      wkhtmltopdf('http://google.com/', { pageSize: 'A4', javascriptDelay: 1000 })
-  		  .pipe(res, 'binary');
+  });
 
-      res.writeHead(200, {
-        'Content-Type': 'application/pdf',
-        'Content-Disposition': 'attachment; filename=test.pdf'
-      });
-    }
-
-    Song.remoteMethod(
-        'pdf',
-        {
-          accepts: { arg: 'res', type: 'object', http: {source: 'res'} },
-          returns: { arg: 'res', type: 'buffer'},
-          http: {path: '/pdf', verb: 'get'}
-        }
-    );
->>>>>>> origin/master
 };
