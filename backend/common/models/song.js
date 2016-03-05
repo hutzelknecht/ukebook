d5c35f08f9a27c30db2a2efce4e91a33462445af
hutzelknecht <micha@immerdieses.de>
2016-03-05 13:50:16 +0100

    added parameter to print command for alternative layout

--- backend/common/models/song.js
+++ backend/common/models/song.js
@@ -1,25 +1,27 @@
 var wkhtmltopdf = require('wkhtmltopdf');
 
 module.exports = function(Song) {
-  Song.pdf = function(res, callback){
-
+  Song.pdf = function(res, layout, callback){
+    var url = 'http://127.0.0.1:3000/#/songbook';
+    if (layout) { url+= layout; }
     res.type('application/pdf');
     res.attachment('songbook.pdf');
-    wkhtmltopdf('http://localhost:3000/#/songbook', {
-      //pageSize: 'A4',
-      //encoding: 'binary',
-      JavascriptDelay: 1000,
+    wkhtmltopdf(url, {
+      JavascriptDelay: 1200,
       outlineDepth: 2,
       printMediaType: true,
       debug: false,
-      cover: 'assets/titlepage.html',
-      toc: ['--xsl-style-sheet assets/toc.xml']
+      cover: '/root/uke/backend/assets/titlepage.html',
+      toc: ['--xsl-style-sheet /root/uke/backend/assets/toc.xml']
     }).pipe(res, 'binary');
 
   };
   Song.remoteMethod('pdf',{
-      accepts: [ { arg: 'res', type: 'object', 'http': {source: 'res'} } ],
-      http: {path: '/pdf', verb: 'get'}
+    accepts: [
+      { arg: 'res', type: 'object', 'http': {source: 'res'} },
+      { arg: 'layout', type: 'string', 'http': {source: 'query'} }
+    ],
+    http: {path: '/pdf', verb: 'get'}
   });
 
 };
